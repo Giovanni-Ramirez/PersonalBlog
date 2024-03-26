599f83a8fd65e22efcb6b774193eeb5e538ca703
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-03-26 13:53:15 -0400

    finish the logic and now styling

--- assets/JS/blog.js
+++ assets/JS/blog.js
@@ -1,15 +1,27 @@
 const posts = localStorage.getItem('posts');
+const section = document.querySelector('section');
 
-function displayPost () {
+function displayPosts () {
     const postsArray = JSON.parse(posts);
 
     for (let i = 0; i < postsArray.length; i++) {
         const divPost = document.createElement('div');
         divPost.className = "divPost";
 
-        const h3Title = document.createElement('h3');
-        h3Title.textContent = postsArray[i].title;
-        
-    }
+        const h2Title = document.createElement('h2');
+        h2Title.textContent = postsArray[i].title;
+        divPost.append(h2Title);
 
-}
+        const pContext = document.createElement('p');
+        pContext.textContent = postsArray[i].context;
+        divPost.append(pContext);
+
+        const h3Author = document.createElement('h3');
+        h3Author.textContent = "by: " + postsArray[i].author;
+        divPost.append(h3Author);
+
+        section.append(divPost);
+    }
+};
+
+displayPosts();
